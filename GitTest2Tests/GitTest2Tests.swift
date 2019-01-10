b3841072bffd8cad433e4423cf48fc77e70e6707
Quentin Turner <q21turner@gmail.com>
2019-01-10 13:51:07 -0500

    Initial Commit

---
+++ GitTest2Tests/GitTest2Tests.swift
@@ -0,0 +1,34 @@
+//
+//  GitTest2Tests.swift
+//  GitTest2Tests
+//
+//  Created by Concetta Turner on 1/10/19.
+//  Copyright © 2019 Integral Mobile AR. All rights reserved.
+//
+
+import XCTest
+@testable import GitTest2
+
+class GitTest2Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
